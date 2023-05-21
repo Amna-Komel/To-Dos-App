976c9c1ed1f3f42c883b53fcdc26292ae65f657b
Amna-Komel <130138586+Amna-Komel@users.noreply.github.com>
2023-05-21 17:56:25 +0500

    Added the add task funtionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './Components/Header/Header'
+import Structure from './Components/Structure/Structure';
+import Add from './Components/Add/Add';
+import Edit from './Components/Edit/Edit';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
-      <Header/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/"element={<Structure tasks={tasks} />}/>
+          <Route path="/create" element={<Add addTask={addTask} />}/>
+          <Route path="update/:id" element={<Edit/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Structure/Structure.js
+++ src/Components/Structure/Structure.js
@@ -1,50 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+function Structure({ tasks }) {
+  const [records, setRecords] = useState([]);
 
-function Structure() {
-  const [columns, setColumns] = useState([])
-  const [records, setRecords] = useState([])
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        setRecords([...tasks, ...res.data]); // Prepend tasks to existing records
+      })
+      .catch((error) => console.log(error));
+  }, [tasks]);
 
-  useEffect(()=> {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then(res => {
-      setColumns(Object.keys(res.data[0]))
-      setRecords(res.data)
-    })
-  }, [])
   return (
     <>
-    <div>
-      <Header/>
-    </div>
-    <div className='container mt-5'>
-      <table className='table'>
-        <thead>
-          <tr>
-            {columns.map((c, i)=>(
-              <th key = {i}>{c}</th>
-            ))}
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            records.map((d, i)=>(
+      <div className="container mt-5">
+        <div className="text-end">
+          <Link to="./create" className="btn btn-primary">Add +</Link>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Completed</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.reverse().map((d, i) => (
               <tr key={i}>
-                <td>{d.userId}</td>
-                <td>{d.id}</td>
                 <td>{d.title}</td>
-                <td>{d.completed ? 'Completed' : 'Pending'}</td>
-                <td>Update/delete</td>
+                <td>{d.completed ? (<input type="checkbox" checked />) : (<input type="checkbox" />)}</td>
+                <td>
+                  <Link to= {`/update/${d.id}`} className='btn btn-sm btn-success'>Update</Link>
+                  <Link to='/delete' className='btn btn-sm ms-1 btn-danger'>Delete</Link>
+                </td>
               </tr>
-            ))
-          }
-        </tbody>
-      </table>
-    </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default Structure
+export default Structure;
